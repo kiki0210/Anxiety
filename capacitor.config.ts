e2b6e3b0b6d550dd8b6fd991cf6ee6a2981b6026
Francesca Bellonzi <fbellonzi@ibc.it>
2026-01-28 15:11:02 +0100

    First Commit React Site + Capacitor app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fbellonzi.anxiety',
+  appName: "Aiuto ho l'ansia",
+  webDir: 'build'
+};
+
+export default config;
--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from './logo.svg';
+import AnxietyReliefTool from './components/anxiety-relief-tool';
 import './App.css';
 
 function App() {
-  return (
+ return (
+    <div>
+      <AnxietyReliefTool />
+    </div>
+  );
+  /**(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +24,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  );*/
 }
 
 export default App;
